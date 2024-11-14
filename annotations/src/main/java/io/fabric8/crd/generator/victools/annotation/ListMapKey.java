9458f4efda7c73284ec4fe8012e4a739e701578a
Bernhard Strähle <bernhard.straehle@gmail.com>
2024-11-14 20:44:16 +0100

    Add javadoc and more test cases
    Signed-off-by: Bernhard Strähle <bernhard.straehle@gmail.com>

--- annotations/src/main/java/io/fabric8/crd/generator/victools/annotation/ListMapKey.java
+++ annotations/src/main/java/io/fabric8/crd/generator/victools/annotation/ListMapKey.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a field as key.
+ * Only considered if {@link ListType.Type#MAP} is used
+ * on the parent container.
+ *
+ * @see ListType
+ */
 @Target({ ElementType.FIELD, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ListMapKey {
--- test/src/test/java/io/fabric8/crd/generator/victools/approvaltests/listtype/ListTypeSpec.java
+++ test/src/test/java/io/fabric8/crd/generator/victools/approvaltests/listtype/ListTypeSpec.java
@@ -21,6 +21,30 @@ public class ListTypeSpec {
   @ListType(ListType.Type.MAP)
   private List<EntityWithTwoKeys> listTypeMapWithTwoKeys;
 
+  @ListType(value = ListType.Type.MAP, mapKeys = {"id"})
+  private List<NotAnnotated> listTypeMapWithOneKeyNotAnnotated;
+
+  @ListType(value = ListType.Type.MAP, mapKeys = {"id", "name"})
+  private List<NotAnnotated> listTypeMapWithTwoKeyNotAnnotated;
+
+  @ListType(ListType.Type.ATOMIC)
+  private String[] arrayListTypeAtomicString;
+
+  @ListType(ListType.Type.SET)
+  private String[] arrayListTypeSetString;
+
+  @ListType(ListType.Type.MAP)
+  private EntityWithOneKey[] arrayListTypeMapWithOneKey;
+
+  @ListType(ListType.Type.MAP)
+  private EntityWithTwoKeys[] arrayListTypeMapWithTwoKeys;
+
+  @ListType(value = ListType.Type.MAP, mapKeys = {"id"})
+  private NotAnnotated[] arrayListTypeMapWithOneKeyNotAnnotated;
+
+  @ListType(value = ListType.Type.MAP, mapKeys = {"id", "name"})
+  private NotAnnotated[] arrayListTypeMapWithTwoKeyNotAnnotated;
+
   @Data
   private static class EntityWithOneKey {
     @ListMapKey
@@ -33,6 +57,12 @@ public class ListTypeSpec {
     @ListMapKey
     private Integer id;
     @ListMapKey
+    private String name;
+  }
+
+  @Data
+  private static class NotAnnotated {
+    private Integer id;
     private String name;
   }
 
